fca4aa4643e396958bb401c41834d9b92dde4e59
Zach King <zacheking10@gmail.com>
2021-09-30 16:04:11 -0600

    header/navBar component complete

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navBar">
+                <h2>Home</h2>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
